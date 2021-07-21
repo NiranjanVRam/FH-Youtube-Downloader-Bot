6d5143a33a4e1ee41522695486477a655f34af3d
NiranjanVRam <83854040+NiranjanVRam@users.noreply.github.com>
2021-07-21 07:53:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,6 +9,6 @@ async def start(client, message):
         [InlineKeyboardButton(
             "Source Code 😊", url="https://t.me/kurachkanjiedukkatte")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n\nI Am A Youtube Downloader Bot developed By @fileshomeofficial \n\n❤Join Our Channel For More...\n\nClick /help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n\nI Am A Youtube Downloader Bot developed By @fileshomeofficial \n\n❤ Join Our Channel For More...\n\nClick /help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
