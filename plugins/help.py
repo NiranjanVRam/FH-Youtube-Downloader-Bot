b9a9a214290547c7b75b75b4d87b58e081313d16
NiranjanVRam <83854040+NiranjanVRam@users.noreply.github.com>
2021-07-21 08:17:57 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Singles(No playlist)\n\nNow send the youtube url of video to be downloaded...\n\n© @fileshomeofficial"
+    helptxt = f"Currently Only supports Youtube Singles(No playlist)\n\nThis Bot Downloads Yt Links As Video.. If You Are Looking For Music downloader, [Click Here](https://t.me/fh_ytmusicdlbot)\n\nNow send the youtube url of video to be downloaded...\n\n© @fileshomeofficial"
     await message.reply_text(helptxt)
